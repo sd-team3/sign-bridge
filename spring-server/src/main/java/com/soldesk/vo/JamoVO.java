3b3cbe5a355f2795e5543cf43580b26c565e314a
yooseunghwan <shy103024@gmail.com>
2026-07-22 18:07:55 +0900

    자/모음 페이지

---
+++ spring-server/src/main/java/com/soldesk/vo/JamoVO.java
@@ -0,0 +1,50 @@
+package com.soldesk.vo;
+
+public class JamoVO {
+    private Long jamoId;
+    private String jamoChar;
+    private String jamoType;
+    private String jamoName;
+    private String jamoInfo;
+    private String jamoImage;
+
+    
+    public Long getJamoId() {
+        return jamoId;
+    }
+    public void setJamoId(Long jamoId) {
+        this.jamoId = jamoId;
+    }
+    public String getJamoChar() {
+        return jamoChar;
+    }
+    public void setJamoChar(String jamoChar) {
+        this.jamoChar = jamoChar;
+    }
+    public String getJamoType() {
+        return jamoType;
+    }
+    public void setJamoType(String jamoType) {
+        this.jamoType = jamoType;
+    }
+    public String getJamoName() {
+        return jamoName;
+    }
+    public void setJamoName(String jamoName) {
+        this.jamoName = jamoName;
+    }
+    public String getJamoInfo() {
+        return jamoInfo;
+    }
+    public void setJamoInfo(String jamoInfo) {
+        this.jamoInfo = jamoInfo;
+    }
+    public String getJamoImage() {
+        return jamoImage;
+    }
+    public void setJamoImage(String jamoImage) {
+        this.jamoImage = jamoImage;
+    }
+
+    
+}
